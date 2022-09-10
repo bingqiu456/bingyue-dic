02d126b5484af803734f5905077ab26b1c65257a
bingyue <99388013+bingqiu456@users.noreply.github.com>
2022-09-10 20:09:27 +0800

    ⚽更新变量主人和管理员

--- message.py
+++ message.py
@@ -3,6 +3,7 @@
 主要写了%的变量
 """
 
+from nonebot import get_driver
 
 class message(object):
 
@@ -32,3 +33,21 @@ class message(object):
         return int 机器人QQ号
         """
         return str(self.self_id)
+    
+    @staticmethod
+    async def 主人(self):
+        """
+        获取配置文件里的主人
+        self : 消息内容
+        return : str = 主人qq号
+        """
+        return str(get_driver().config.dict().get("owner_dic","你还没设置主人"))
+
+    @staticmethod
+    async def 管理员(self):
+        """
+        获取配置文件里的管理员列表
+        self : 消息内容
+        return : str = 管理员列表
+        """
+        return str(get_driver().config.dict().get("admin_dic","你还没管理员"))
