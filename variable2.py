01855f1631a5404276d0db996e747cc0ffe86878
bingyue <99388013+bingqiu456@users.noreply.github.com>
2022-09-18 20:18:36 +0800

    ✈更新到0.4.1beta版
    更新群头衔和写文件

--- variable2.py
+++ variable2.py
@@ -1,7 +1,7 @@
-import httpx , os , random
+import httpx
+import random
+import os
 from nonebot import get_bot
-from nonebot.adapters.onebot.exception import ActionFailed
-from nonebot.log import logger
 """
 本层是写 bingyuedic 的$变量封装
 查看帮助 还没有
@@ -11,28 +11,23 @@ from nonebot.log import logger
 class Task:
 
     @staticmethod
-    async def 禁(self:str):
+    async def 禁(event,self: str):
         """
         禁言
         self : ["群号","QQ","秒数"]
         return 直接禁言
         """
         self = self.split(maxsplit=2)
-        try:
-            await get_bot().call_api("set_group_ban",**{
-                "group_id":self[0],
-                "user_id":self[1],
-                "duration":self[2],
+        await get_bot().call_api("set_group_ban",**{
+                "group_id": self[0],
+                "user_id": self[1],
+                "duration": self[2],
             })
-        except(ActionFailed):
-            logger.error("禁言错误 这有可能是机器人权限不足引起的")
-            return ""
-        else:
-            return ""
+        return ""
 
 
     @staticmethod
-    async def 读(self:str):
+    async def 读(event,self: str):
         """
         读取文件
         self : ["文件路径","索引","内容"]
@@ -45,45 +40,81 @@ class Task:
                     return lujing[2]
                 else:
                     return e.read()
-        except(FileExistsError,NotADirectoryError):
+        except(FileExistsError, NotADirectoryError):
             return lujing[2]
 
     @staticmethod
-    async def 随机数(self:str):
+    async def 随机数(event,self: str):
         """
         生成随机数
         self : ["随机数最大值","随机数最小值"]
         return int
         """
         self = self.split(maxsplit=1)
-        return str(random.randint(int(self[0]),int(self[1])))
+        return str(random.randint(int(self[0]), int(self[1])))
 
     @staticmethod
-    async def 改(self:str):
+    async def 改(event,self: str):
         """
         读取文件
         self : ["群号","QQ号","昵称"]
         return 失败会返回错误提示 成功则无操作
         """
         self = self.split(maxsplit=2)
-        try:
-            await get_bot().call_api("set_group_card", **{
+        await get_bot().call_api("set_group_card", **{
                 "group_id": self[0],
                 "user_id": self[1],
                 "card": self[2],
             })
-        except(ActionFailed):
-            logger.error("改名错误 这有可能是机器人权限不足引起的")
-            return ""
-        else:
-            return ""
+        return ""
 
 
     @staticmethod
-    async def 访问(url:str):
+    async def 访问(url: str):
         """
         访问外部链接
         url : 网站连接
         return 访问内容
         """
-        return await httpx.AsyncClient().get(url=url,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"}).text
+        return await httpx.AsyncClient().get(url=url,headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36" }).text
+
+    @staticmethod
+    async def 写(event,self: str):
+        """
+        写入配置文件
+        self : [路径,索引,内容]
+        return : ''
+        """
+        o = self.split(maxsplit=2)
+        try:
+            with open(o[0], "w+", encoding="utf_8") as f:
+                if f.read() == '':
+                    k = {}
+                else:
+                    k = json.load(f)
+                k[str(o[1])] = str(o[2])
+                f.write(str(k))
+                f.close()
+        except(FileNotFoundError):
+            v = ' '.join(o[0].split("/")[::-1]).split(maxsplit=1)
+            os.makedirs(v[1].replace(" ", "/"))
+            with open(o[0], "w+", encoding="utf_8") as f:
+                if f.read() == '':
+                    k = {}
+                else:
+                    k = json.load(f)
+                k[str(o[1])] = str(o[2])
+                f.write(str(k))
+                f.close()
+        return ''
+
+    @staticmethod
+    async def 群头衔(event,self: str):
+        self = self.split(maxsplit=2)
+        await get_bot().call_api("set_group_special_title", **{
+            "group_id": self[0],
+            "user_id": self[1],
+            "special_title": self[2],
+        })
+        return ''
+
